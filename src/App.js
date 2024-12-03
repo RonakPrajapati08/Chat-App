d4b194ca737cf5973d63d71d40f9059b1b963df8
RonakPrajapati08 <ronakprajapati3545@gmail.com>
2024-12-03 18:42:27 +0530

    chat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
 // App.js
-// import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import LoginPage from "./components/LoginPage";
-// import SignUp from "./components/SignUp";
-// import ChatPage from "./components/ChatPage";
-
-// function App() {
-//   return (
-//     <Router basename="/Chat-App">
-//       <Routes>
-//         <Route path="/" element={<LoginPage />} />
-//         <Route path="/signup" element={<SignUp />} />
-//         <Route path="/chat/:userId" element={<ChatPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
 import SignUp from "./components/SignUp";
 import ChatPage from "./components/ChatPage";
