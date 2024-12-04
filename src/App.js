08119831dbbc3d98b533c7ac993be01ff5e6a741
RonakPrajapati08 <ronakprajapati3545@gmail.com>
2024-12-04 14:35:05 +0530

    chat

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 // App.js
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LoginPage from "./components/LoginPage";
-import SignUp from "./components/SignUp";
-import ChatPage from "./components/ChatPage";
-
-function App() {
-  return (
-    <Router basename="/Chat-App">
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/chat" element={<ChatPage />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-
 // import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-//   Navigate,
-// } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import LoginPage from "./components/LoginPage";
 // import SignUp from "./components/SignUp";
 // import ChatPage from "./components/ChatPage";
@@ -37,13 +12,38 @@ export default App;
 //         <Route path="/" element={<LoginPage />} />
 //         <Route path="/signup" element={<SignUp />} />
 //         <Route path="/chat/:userId" element={<ChatPage />} />
-//         <Route path="*" element={<Navigate to="/" replace />} />
 //       </Routes>
 //     </Router>
 //   );
 // }
 
 // export default App;
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import SignUp from "./components/SignUp";
+import ChatPage from "./components/ChatPage";
+
+function App() {
+  return (
+    <Router basename="/Chat-App">
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
 // import React, { useState } from "react";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
